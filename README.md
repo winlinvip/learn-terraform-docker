b3e899a63278ad731279a5ff214b62c88c2b801b
winlin <winlin@vip.126.com>
2022-01-03 12:26:41 +0800

    Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # learn-terraform-docker
 
 Deploy SRS by [terraform docker](https://learn.hashicorp.com/tutorials/terraform/infrastructure-as-code?in=terraform/docker-get-started).
+
+* [install](install): Deploy SRS by [Install Terraform](https://learn.hashicorp.com/tutorials/terraform/install-cli?in=terraform/docker-get-started)
+
---
+++ install/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "srs" {
+  name = "ossrs/srs:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "srs" {
+  image = docker_image.srs.latest
+  name  = "srs"
+  ports {
+    internal = 8080
+    external = 8080
+    protocol = "tcp"
+  }
+  ports {
+    internal = 1985
+    external = 1985
+    protocol = "tcp"
+  }
+  ports {
+    internal = 1935
+    external = 1935
+    protocol = "tcp"
+  }
+}
